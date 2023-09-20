2ef6faff68a6d0623aa68558be334a2504997610
miguel <91511269+ghmacg@users.noreply.github.com>
2023-09-20 11:34:43 -0400

    Create Book class and Library class

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+class Book {
+    constructor(
+        title='Unknown',
+        author='Unknown',
+        pages='0',
+        isRead=false
+    ) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isRead = isRead;
+    }
+}
+
+class Library {
+    constructor() {
+        this.books = [];
+    }
+}
