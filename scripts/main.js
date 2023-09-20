fc65bb2c282e2b9f7847855fafc914d80a0c7086
miguel <91511269+ghmacg@users.noreply.github.com>
2023-09-20 13:09:48 -0400

    Create function to display each book in library

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+// Data structures
 class Book {
     constructor(
         title='Unknown',
@@ -8,7 +9,7 @@ class Book {
         this.title = title;
         this.author = author;
         this.pages = pages;
-        this.isRead = isRead;
+        this.isRead = isRead ? 'Read' : 'Not Read';
     }
 }
 
@@ -20,4 +21,33 @@ class Library {
     addBook(newBook) {
         this.books.push(newBook);
     }
+}
+
+// Display
+const cardContainer = document.getElementById('card-container');
+
+function showBooks(library) {
+    library.books.forEach(book => {
+        const card = document.createElement('div');
+        const title = document.createElement('div');
+        const author = document.createElement('div');
+        const pages = document.createElement('div');
+        const isReadBtn = document.createElement('button');
+        const removeBtn = document.createElement('button');
+        
+        card.classList.add('card');
+        
+        title.innerHTML = `"${book.title}"`;
+        author.innerHTML = book.author;
+        pages.innerHTML = `${book.pages} pages`;
+        isReadBtn.innerHTML = book.isRead;
+        removeBtn.innerHTML = 'Remove';
+        
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
+        card.appendChild(isReadBtn);
+        card.appendChild(removeBtn);
+        cardContainer.appendChild(card);
+    })
 }
