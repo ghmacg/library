2b449f18b6e33535102031214b774b87c403a1fb
miguel <91511269+ghmacg@users.noreply.github.com>
2023-09-20 21:44:38 -0400

    Create add and remove books functions in library class

--- scripts/main.js
+++ scripts/main.js
@@ -10,44 +10,20 @@ class Book {
         this.author = author;
         this.pages = pages;
         this.isRead = isRead ? 'Read' : 'Not Read';
-    }
-}
+    };
+};
 
 class Library {
     constructor() {
         this.books = [];
-    }
+    };
 
-    addBook(newBook) {
-        this.books.push(newBook);
-    }
-}
+    addBook(book) {
+        this.books.push(book);
+    };
 
-// Display
-const cardContainer = document.getElementById('card-container');
-
-function showBooks(library) {
-    library.books.forEach(book => {
-        const card = document.createElement('div');
-        const title = document.createElement('div');
-        const author = document.createElement('div');
-        const pages = document.createElement('div');
-        const isReadBtn = document.createElement('button');
-        const removeBtn = document.createElement('button');
-        
-        card.classList.add('card');
-        
-        title.innerHTML = `"${book.title}"`;
-        author.innerHTML = book.author;
-        pages.innerHTML = `${book.pages} pages`;
-        isReadBtn.innerHTML = book.isRead;
-        removeBtn.innerHTML = 'Remove';
-        
-        card.appendChild(title);
-        card.appendChild(author);
-        card.appendChild(pages);
-        card.appendChild(isReadBtn);
-        card.appendChild(removeBtn);
-        cardContainer.appendChild(card);
-    })
-}
+    removeBook(book) {
+        const index = Number(book.dataset.indexNumber);
+        this.books.splice(index, 1);
+    };
+};
